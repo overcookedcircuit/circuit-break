6f1a4b7217d9b0db1dfa6e4251030fa0326a9c42
overcookedcircuit <manaspatel31122004@gmail.com>
2025-10-24 01:33:06 -0400

    player walk animation left and right

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
+var current_dir = "none"
 
 
 func _physics_process(delta):
@@ -10,9 +11,13 @@ func _physics_process(delta):
 
 func player_movement(delta):
 	if Input.is_action_pressed("right"):
+		current_dir = "right"
+		play_animation(1)
 		velocity.x = SPEED
 		velocity.y = 0
 	elif Input.is_action_pressed("left"):
+		current_dir = "left"
+		play_animation(1)
 		velocity.x = -SPEED 
 		velocity.y = 0
 	elif Input.is_action_pressed("down"):
@@ -22,7 +27,25 @@ func player_movement(delta):
 		velocity.x = 0
 		velocity.y = -SPEED
 	else:
+		play_animation(0)
 		velocity.x = 0
 		velocity.y = 0
 	
 	move_and_slide()
+
+func play_animation(movement):
+	var dir = current_dir
+	var animation = $AnimatedSprite2D
+	
+	if dir == "right":
+		animation.flip_h = false
+		if movement == 1:
+			animation.play("walking")
+		elif movement == 0:
+			animation.play("idle")
+	if dir == "left":
+		animation.flip_h = true
+		if movement == 1:
+			animation.play("walking")
+		elif movement == 0:
+			animation.play("idle")
