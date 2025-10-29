8cbbf027bc251fc72eda7ea0c4992c35d50dea99
overcookedcircuit <manaspatel31122004@gmail.com>
2025-10-29 02:25:43 -0400

    updated code for running and movements

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,35 +1,47 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
+const WALK_SPEED = 150.0
 const JUMP_VELOCITY = -400.0
-var current_dir = "none"
+const RUN_SPEED = 300.0
 
+var current_dir = "none"
+var is_running = false
+
+func _ready():
+	$AnimatedSprite2D.play("idle")
 
 func _physics_process(delta):
 	player_movement(delta)
 
 func player_movement(delta):
+	is_running = Input.is_action_pressed("shift")
+	
+	var speed = WALK_SPEED
+	if is_running:
+		speed = RUN_SPEED
+	
+	# Horizontal movement
 	if Input.is_action_pressed("right"):
 		current_dir = "right"
 		play_animation(1)
-		velocity.x = SPEED
-		velocity.y = 0
+		velocity.x = speed
 	elif Input.is_action_pressed("left"):
 		current_dir = "left"
 		play_animation(1)
-		velocity.x = -SPEED 
-		velocity.y = 0
-	elif Input.is_action_pressed("down"):
-		velocity.x = 0
-		velocity.y = SPEED
-	elif Input.is_action_pressed("up"):
-		velocity.x = 0
-		velocity.y = -SPEED
+		velocity.x = -speed
 	else:
 		play_animation(0)
 		velocity.x = 0
+	
+	# Vertical movement
+	if Input.is_action_pressed("up"):
+		velocity.y = -speed
+	elif Input.is_action_pressed("down"):
+		velocity.y = speed
+	else:
 		velocity.y = 0
+	
 	
 	move_and_slide()
 
@@ -39,13 +51,13 @@ func play_animation(movement):
 	
 	if dir == "right":
 		animation.flip_h = false
-		if movement == 1:
-			animation.play("walking")
-		elif movement == 0:
-			animation.play("idle")
-	if dir == "left":
+	elif dir == "left":
 		animation.flip_h = true
-		if movement == 1:
+
+	if movement == 1:
+		if is_running:
+			animation.play("running")
+		else:
 			animation.play("walking")
-		elif movement == 0:
-			animation.play("idle")
+	else:
+		animation.play("idle")
